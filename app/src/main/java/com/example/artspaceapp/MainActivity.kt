90ae73b7efcf72e2d036b8114263eeecfab3c125
Peter Gabriel <peternex42@gmail.com>
2023-05-04 23:37:00 +0800

    Add the component not quite good yet, want it to be more attractive in the next commit

--- app/src/main/java/com/example/artspaceapp/MainActivity.kt
+++ app/src/main/java/com/example/artspaceapp/MainActivity.kt
@@ -4,15 +4,31 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.artspaceapp.ui.theme.ArtSpaceAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,9 +51,41 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun SpaceArtApp() {
     Column(
-
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
-        Image(painter = painterResource(id = R.drawable.gabs_foro) , contentDescription = null)
+        Image(
+            painter = painterResource(id = R.drawable.gabs_foro),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .aspectRatio(16f / 15f)
+                .padding(start = 20.dp , end = 20.dp , bottom = 20.dp)
+                .clip(CircleShape)
+        )
+        Text(
+            text = stringResource(id = R.string.gabs)
+        )
+        Text(
+            text = stringResource(id = R.string.gabs_nim)
+        )
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.Bottom
+        ) {
+            Button(onClick = { /*TODO*/ }, modifier = Modifier
+                .weight(1f)
+                .padding(horizontal = 8.dp)) {
+
+            }
+            Button(onClick = { /*TODO*/ }, modifier = Modifier
+                .weight(1f)
+                .padding(horizontal = 8.dp)) {
+
+            }
+        }
     }
 }
 
