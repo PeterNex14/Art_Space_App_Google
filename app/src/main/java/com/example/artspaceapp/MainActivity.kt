cade88a757ce17c3f117622c42cce45fcc25a326
Peter Gabriel <peternex42@gmail.com>
2023-05-04 22:10:13 +0800

    Add Image

--- app/src/main/java/com/example/artspaceapp/MainActivity.kt
+++ app/src/main/java/com/example/artspaceapp/MainActivity.kt
@@ -3,12 +3,15 @@ package com.example.artspaceapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.artspaceapp.ui.theme.ArtSpaceAppTheme
 
@@ -31,7 +34,11 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun SpaceArtApp() {
-    
+    Column(
+
+    ) {
+        Image(painter = painterResource(id = R.drawable.gabs_foro) , contentDescription = null)
+    }
 }
 
 @Preview(showBackground = true)
