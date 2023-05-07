2270ed50c6430861ba567c76deba59e7f2c575c6
Peter Gabriel <peternex42@gmail.com>
2023-05-07 21:13:36 +0800

    Add Image border and shadow

--- app/src/main/java/com/example/artspaceapp/MainActivity.kt
+++ app/src/main/java/com/example/artspaceapp/MainActivity.kt
@@ -3,12 +3,15 @@ package com.example.artspaceapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -24,6 +27,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -51,18 +56,20 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun SpaceArtApp() {
     Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+        modifier = Modifier.fillMaxHeight(),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Image(
             painter = painterResource(id = R.drawable.gabs_foro),
             contentDescription = null,
             contentScale = ContentScale.Crop,
             modifier = Modifier
-                .aspectRatio(16f / 15f)
-                .padding(start = 20.dp , end = 20.dp , bottom = 20.dp)
-                .clip(CircleShape)
+                .shadow(5.dp)
+                .border(BorderStroke(20.dp , Color.White))
+                .size(290.dp)
+                .clip(RoundedCornerShape(16.dp))
         )
+        Spacer(modifier = Modifier.height(10.dp))
         Text(
             text = stringResource(id = R.string.gabs)
         )
@@ -72,8 +79,7 @@ fun SpaceArtApp() {
 
         Row(
             modifier = Modifier
-                .fillMaxWidth(),
-            verticalAlignment = Alignment.Bottom
+                .fillMaxWidth()
         ) {
             Button(onClick = { /*TODO*/ }, modifier = Modifier
                 .weight(1f)
